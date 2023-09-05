9cc35128487f5f5a8b6483e3f72661eb2771c12f
shahinalam02 <shahinalam982.as@gmail.com>
2023-09-05 23:37:22 +0600

    first commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,75 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What gets wetter as it dries?',
+    [
+      'A towel',
+      'A sponge',
+      'A shirt',
+      'A book',
+    ],
+  ),
+  QuizQuestion(
+      "I have keys but open no locks. I have space but no room. You can enter, but you can't go inside. What am I?",
+      [
+        'A keyboard',
+        'A smartphone',
+        'A car',
+        'A house',
+      ]),
+  QuizQuestion(
+    'The more you take, the more you leave behind. What am I?',
+    [
+      'Footprints',
+      'Memories',
+      'Luggage',
+      'Money',
+    ],
+  ),
+  QuizQuestion(
+    'What can travel around the world while staying in a corner?',
+    [
+      'A stamp',
+      'A plane',
+      'A soccer ball',
+      'A magician',
+    ],
+  ),
+  QuizQuestion(
+    "I'm not alive, but I can grow; I don't have lungs, but I need air. What am I?",
+    [
+      'Fire',
+      'A volcano',
+      'A plant',
+      'A cloud',
+    ],
+  ),
+  QuizQuestion(
+    'What can you catch but not throw?',
+    [
+      'A cold',
+      'A ball',
+      'A fish',
+      'A nap',
+    ],
+  ),
+  QuizQuestion(
+    'What begins and has no end?',
+    [
+      'A circle',
+      'A river',
+      'A book',
+      'A movie',
+    ],
+  ),
+  QuizQuestion(
+    'I speak without a mouth and hear without ears. What am I?',
+    [
+      'An echo',
+      'A book',
+      'A phone',
+      'A television',
+    ],
+  ),
+];
